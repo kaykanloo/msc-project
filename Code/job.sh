2fffbb23aaa50a31bbedd374e769e659de639c30
M Kaykanloo <sc16mk@login1.arc3.leeds.ac.uk>
2017-08-20 22:04:58 +0100

    Adding job.sh

---
+++ Code/job.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export PATH="/nobackup/sc16mk/miniconda3/bin:$PATH"
+export PATH="/nobackup/sc16mk/cuda-8.0/bin:$PATH"
+export LD_LIBRARY_PATH="/nobackup/sc16mk/cuda-8.0/lib64:$LD_LIBRARY_PATH"
+
+python Training.py
