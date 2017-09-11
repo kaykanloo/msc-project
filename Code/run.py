d1a7057a00eef4941f2396ef64a82e492b2bf415
Mohammad Kaykanloo <kaykanloo@gmail.com>
2017-09-11 11:51:14 +0100

    Checking for file over-writing.

--- Code/run.py
+++ Code/run.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import configparser
 from Experiments.Training import Train
@@ -12,17 +13,38 @@ if __name__ == '__main__':
     # Reading the config File
     config = configparser.ConfigParser()
     config.read('Experiments/ConfigFiles/'+args.ConfigID+'.ini')
+    # Output directory
+    if not os.path.exists('Experiments/Outputs/'):
+        os.makedirs('Experiments/Outputs/')
+    # MAT directory
+    if not os.path.exists('DataSets/MAT/'):
+        os.makedirs('DataSets/MAT/')
+        print('Warning: No data set is found in DataSets/MAT/')
     # Training
     if (args.Command == 'training') and ('TRAINING' in config):
-        exec('from DataSets.'+config['TRAINING']['dataset']+' import Dataset')
-        exec('from Models.'+config['TRAINING']['model']+' import model')
-        Train(args.ConfigID, Dataset, model, config['TRAINING']['loss'], config['TRAINING']['optimizer'], int(config['TRAINING']['batchsize']), int(config['TRAINING']['epochs']))
+        if os.path.exists('Experiments/Outputs/'+ args.ConfigID + '.h5'):
+            print('Trained model (Experiments/Outputs/'+ args.ConfigID + '.h5)'+' already exists. Please (re)move this file before trying again.')
+        else:
+            if not os.path.exists('Experiments/Outputs/'):
+                os.makedirs('Experiments/Outputs/')
+            exec('from DataSets.'+config['TRAINING']['dataset']+' import Dataset')
+            exec('from Models.'+config['TRAINING']['model']+' import model')
+            Train(args.ConfigID, Dataset, model, config['TRAINING']['loss'], config['TRAINING']['optimizer'], int(config['TRAINING']['batchsize']), int(config['TRAINING']['epochs']))
     # Prediction
     elif (args.Command == 'prediction') and ('PREDICTION' in config):
-        exec('from DataSets.'+config['PREDICTION']['dataset']+' import Dataset')
-        Predict(args.ConfigID, Dataset)
+        clean = True # No file is left from past predictions
+        if os.path.exists('Experiments/Outputs/'+ args.ConfigID + '.mat'):
+            clean = False
+            print('Predicted normals file (Experiments/Outputs/'+ args.ConfigID + '.mat)'+' already exists. Please (re)move this file before trying again.')
+        if os.path.exists('Experiments/Outputs/'+args.ConfigID+'/'):
+            clean = False
+            print('Predicted normal maps (Experiments/Outputs/'+ args.ConfigID + '/)'+' already exist. Please (re)move this directory before trying again.')
+        if clean:
+            os.makedirs('Experiments/Outputs/'+args.ConfigID+'/')
+            exec('from DataSets.'+config['PREDICTION']['dataset']+' import Dataset')
+            Predict(args.ConfigID, Dataset)
     # Evaluation
     elif (args.Command == 'evaluation') and ('EVALUATION' in config):
-        print('Evaluation')
+        print('For evaluation run the matlab script.')
     else:
         print("Invalid command or config file.")
